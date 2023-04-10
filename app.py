fff5746140dafab9f4af8a88263a0fa7392035cd
tonghao <tong-hao@hotmail.com>
2023-04-10 17:31:52 +0800

    bookmark render

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+bookmarks = [
+    {'id': 1, 'title': 'Google', 'url': 'http://www.google.com'},
+    {'id': 2, 'title': 'Yahoo', 'url': 'http://www.yahoo.com'},
+    {'id': 3, 'title': 'bing', 'url': 'http://www.bing.com'},
+]
+
+
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html', bookmarks=bookmarks)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
